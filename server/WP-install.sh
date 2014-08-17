7ed9764c229937efe55a1d89d13462f8abf6f811
Aris <aris@papatheodorou.net>
2014-08-17 17:02:15 +0200

    More consistency for comment and dialogs

--- server/WP-install.sh
+++ server/WP-install.sh
@@ -2,7 +2,7 @@
 # 
 # wp-install.sh - v1.0 (17/09/2014)
 # Aris Papathéodorou
-# Script d’installation de WordPress en français
+# Script interactif d’installation de WordPress en français
 # License : domaine public
 #
 # Fonctions :
@@ -16,26 +16,32 @@
 # - Configuration du fichier : wp-config.php
 #
 # Utilisation :
-# Le script doit être copié à la racine du répertoire utilisateur
+# Le script doit être copié à la racine du répertoire utilisateur (/home/<user>/)
 # Vérifier que le fichier est bien exécutable. Sinon : chmod u+x wp-install.sh
 # Lancer : ./wp-install.sh
 #
+# Attention : 
+# - Testé uniquement avec des version 3.9.* de WordPress
+# - Si l’on n’est ni sur Ubuntu, ni sur Debian, dans ce cas le groupe d’Apache 
+#   n’est pas www-data il faut donc adapter ce script (ex. : RedHat = apache)
+# - L'utilisateur doit appartenir au même groupe qu’Apache
+#
 
-echo "Version de WordPress à installer"
+echo "Version de WordPress à installer (ex. : 3.9.2)"
 read wpversion
 echo "Téléchargement de WordPress"
 curl -o wordpress.tar.gz http://fr.wordpress.org/wordpress-$wpversion-fr_FR.tar.gz
 tar -xvzf wordpress.tar.gz
 rm -f wordpress.tar.gz
 
-echo "Répertoire d’installation"
+echo "Répertoire d’installation (ex. : public_html ou htdocs)"
 read htmlforder
 echo " "
 echo "Installation de WordPress"
 mv wordpress/ $htmlforder/
 cp -p $htmlforder/wp-config-sample.php $htmlforder/wp-config.php
 
-echo "Nom de l’utilisateur (compte)"
+echo "Nom du compte utilisateur (ex. : toto)"
 read username
 echo " "
 echo "Finalisation de l’installation"
@@ -45,8 +51,10 @@ cd $htmlforder/
 find . -exec chown $username:www-data {} +
 find . -type f -exec chmod 664 {} +
 find . -type d -exec chmod 775 {} +
+# Attention : peut ne pas fonctionner avec toutes les configurations serveur dans ce cas, 
+# commenter l'instruction ci-dessous, et le fichier wp-config.php restera en 664
 chmod 660 wp-config.php
 cd ..
 
 echo " "
-echo "Installation réussie de WordPress $wpversion dans $htmlforder"
+echo "L’nstallation de WordPress $wpversion, dans $htmlforder/, est terminée"
